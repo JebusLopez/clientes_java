8ae8b5231710657130d034a6bfdd315fe1f221f0
JebusLopez <50957364+JebusLopez@users.noreply.github.com>
2020-02-24 08:27:19 -0600

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,46 @@
+import java.awt.Frame;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        final String HOST = "35.170.242.152";
+        final int PORT = 5000;
+        DataInputStream in;
+        DataOutputStream out;
+        
+        try {
+            //Socket para conectar con el servidor
+            Socket sc = null; 
+            int opc;
+            sc = new Socket(HOST, PORT);
+            in = new DataInputStream(sc.getInputStream());
+            out = new DataOutputStream(sc.getOutputStream());
+            Scanner entrada = new Scanner(System.in);
+            String data = "";
+            System.out.println("CONVERTIDOR DE CADENAS/ 'salir' para finalizar");
+            while(data != "salir"){
+                System.out.println("Ingresa la cadena a convertir");
+                data = entrada.nextLine();
+                out.writeUTF(data);
+                String mensaje = in.readUTF();
+                System.out.println("Resultado: " + mensaje+"\n");
+            }
+            out.close();
+            in.close();
+            sc.close();
+        }catch (IOException ex) {
+            println("Error: "+ ex);
+        }
+    }
+    
+}
